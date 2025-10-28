565e195aaa0ce8260ae4aac70c24a768f15d4ce9
Explorarthecreator <fredadejiemma99@gmail.com>
2025-10-28 20:17:09 +0100

    feat: experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Experience from "@/components/section/experience";
 import Hero from "@/components/section/hero";
 
 export default function Home() {
   return (
     <div className=" px-6 md:px-10 xl:px-0 max-w-[1288px] m-auto">
       <Hero />
+
+      <Experience />
     </div>
   );
 }
